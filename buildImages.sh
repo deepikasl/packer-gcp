3df1a063014365d463df774a693e65177154a4f9
Jatin Dogra <dogra.jatin5@gmail.com>
2020-03-03 19:16:26 +0530

    Update buildImages.sh

--- buildImages.sh
+++ buildImages.sh
@@ -21,8 +21,8 @@ distribute_images() {
   echo "distributing images"
   echo "-----------------------------------"
 
-  gcloud compute images create u16-poc-asia-east1 --source-image u16-poc-us-west1 --source-image-project gcp-dn --storage-location asia-east1
-  gcloud compute images create u16-poc-europe-west2 --source-image u16-poc-us-west1 --source-image-project gcp-dn --storage-location europe-west2
+  gcloud compute images create u16-poc-asia-east1 --source-image u16-poc-us-west1 --source-image-project gcp-dn-poc --storage-location asia-east1
+  gcloud compute images create u16-poc-europe-west2 --source-image u16-poc-us-west1 --source-image-project gcp-dn-poc --storage-location europe-west2
 }
 
 make_image_public() {
